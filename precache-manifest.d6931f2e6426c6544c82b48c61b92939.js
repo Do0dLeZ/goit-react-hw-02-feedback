0ad4250f9fd37ed688eafd9ba4ae0676ac4830c2
Eugene Kolomiiets <kolomiets1990@gmail.com>
2020-10-01 13:56:56 +0300

    Updates

---
+++ precache-manifest.d6931f2e6426c6544c82b48c61b92939.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d82523f7039d18902fe569633213d8d5",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "dfecf363b9111312fdaa",
+    "url": "/goit-react-hw-02-feedback/static/js/2.18cad9f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.18cad9f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b135644806db863e84d5",
+    "url": "/goit-react-hw-02-feedback/static/js/main.a7925942.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
